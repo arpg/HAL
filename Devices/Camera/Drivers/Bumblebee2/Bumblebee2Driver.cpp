d4689b9abf7c72e5a71c8cbb1c4e1c67a12c80c9
Juan Falquez <jfalquez@gwmail.gwu.edu>
2011-09-19 20:50:00 +0000

    BB driver
    git-svn-id: svn+ssh://robotics.gwu.edu/home/rpg/repos/RPG/core/RPG@8 e56edf37-dbb1-46d4-901c-b18800ef280d

--- Devices/Camera/Drivers/Bumblebee2/Bumblebee2Driver.cpp
+++ Devices/Camera/Drivers/Bumblebee2/Bumblebee2Driver.cpp
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #include "Bumblebee2Driver.h"
 #include <Mvlpp/Utils.h>  // for FindFiles and PrintError
+#include <dc1394/conversions.h>
 #include "opencv/cv.h"	// for Mat structure
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -32,6 +33,7 @@ Bumblebee2Driver::~Bumblebee2Driver()
 {
 }
 
+
 ///////////////////////////////////////////////////////////////////////////////
 bool Bumblebee2Driver::Capture( std::vector<cv::Mat>& vImages )
 {
@@ -40,6 +42,9 @@ bool Bumblebee2Driver::Capture( std::vector<cv::Mat>& vImages )
     if( vImages.size() != 2 ){
         vImages.resize( 2 ); 
         // and setup images
+//        vImages[0] = cv::Mat(m_nImageHeight*2, m_nImageWidth, CV_8UC1);
+        vImages[0] = cv::Mat(m_nImageHeight/2, m_nImageWidth/2, CV_8UC1);
+        vImages[1] = cv::Mat(m_nImageHeight/2, m_nImageWidth/2, CV_8UC1);
     }
     // should also check images are the right size, type etc
 
@@ -49,9 +54,26 @@ bool Bumblebee2Driver::Capture( std::vector<cv::Mat>& vImages )
     e = dc1394_capture_dequeue( m_pCam, DC1394_CAPTURE_POLICY_WAIT, &pFrame );
     DC1394_ERR_CLN_RTN(e, cleanup_and_exit(m_pCam),"Could not capture a frame");
 
+    uint8_t buff[m_nImageWidth*m_nImageHeight*2];
+    // trying stuff
+    dc1394_deinterlace_stereo( pFrame->image, buff, m_nImageWidth, m_nImageHeight*2 );
+
     // copy to our buffer, split, decimate, convert, etc.
-    // fill vImage[0]
-    // vfill Image[1]
+    int a,b,c,d;
+    for( int ii = 0; ii < m_nImageHeight; ii+=2 ) {
+    	for( int jj = 0; jj < m_nImageWidth; jj+=2 ) {
+    		a = buff[ (ii * m_nImageWidth) + jj   ];
+    		b = buff[ (ii * m_nImageWidth) + jj + 1 ];
+    		c = buff[((ii + 1) * m_nImageWidth) + jj ];
+    		d = buff[((ii + 1) * m_nImageWidth) + jj + 1 ];
+    		vImages[0].data[ ((ii/2)*(m_nImageWidth/2))+(jj/2) ] = ( a + b + c + d) / 4;
+    		a = buff[ ((ii + m_nImageHeight) * m_nImageWidth) + jj   ];
+    		b = buff[ ((ii + m_nImageHeight) * m_nImageWidth) + jj + 1 ];
+    		c = buff[(((ii + m_nImageHeight) + 1) * m_nImageWidth) + jj ];
+    		d = buff[(((ii + m_nImageHeight) + 1) * m_nImageWidth) + jj + 1 ];
+    		vImages[1].data[ ((ii/2)*(m_nImageWidth/2))+(jj/2) ] = ( a + b + c + d) / 4;
+    	}
+    }
 
     // release the frame
     e = dc1394_capture_enqueue( m_pCam, pFrame );
@@ -85,18 +107,16 @@ bool Bumblebee2Driver::Init()
 
     // free the camera lsit
     dc1394_camera_free_list( pCameraList );
-    printf("Using camera with GUID %d\n", m_pCam->guid );
+    printf("Using camera with GUID %llu\n", m_pCam->guid );
 
     // always this
     m_nVideoMode = DC1394_VIDEO_MODE_FORMAT7_3;
 
     dc1394color_coding_t coding;
-    e = dc1394_get_color_coding_from_video_mode( m_pCam,m_nVideoMode, &coding );
+    e = dc1394_get_color_coding_from_video_mode( m_pCam, m_nVideoMode, &coding );
     DC1394_ERR_CLN_RTN(e,cleanup_and_exit(m_pCam),"Could not get color coding");
 
     // capure an initial image to get the image sizes (SetProperty)
-
-    // checout dc1394_feature_print_all!! cframerateool function
 
     // get highest framerate
 //    dc1394framerates_t vFramerates;
@@ -118,6 +138,7 @@ bool Bumblebee2Driver::Init()
             DC1394_CAPTURE_FLAGS_DEFAULT );
     DC1394_ERR_CLN_RTN(e,cleanup_and_exit(m_pCam),"Could not setup camera. make sure that the video mode and framerate are supported by your camera.");
 
+    // print camera features
     e = dc1394_feature_get_all( m_pCam, &m_vFeatures );
     if( e != DC1394_SUCCESS ) {
         dc1394_log_warning("Could not get feature set");
@@ -126,6 +147,7 @@ bool Bumblebee2Driver::Init()
         dc1394_feature_print_all( &m_vFeatures, stdout );
     }
 
+    // initiate transmission
     e = dc1394_video_set_transmission( m_pCam, DC1394_ON );
     DC1394_ERR_CLN_RTN( e, cleanup_and_exit(m_pCam),"Could not start camera iso transmission");
 
@@ -136,10 +158,16 @@ bool Bumblebee2Driver::Init()
 
 
     // copy to our buffer, decimate, convert, etc.
-
-
-
-
+    // print image information
+    m_nImageWidth = pFrame->size[0];
+    m_nImageHeight = pFrame->size[1];
+    printf("\nIMAGE INFORMATION:\n");
+    printf("------------------------\n");
+    printf("Image Size: %d x %d\n", m_nImageWidth, m_nImageHeight );
+    printf("Data Depth: %d\n", pFrame->data_depth );
+    printf("Stride: %d\n", pFrame->stride );
+    printf("Total Bytes: %llu\n", pFrame->total_bytes );
+    printf("------------------------\n");
 
     // release the frame
     e = dc1394_capture_enqueue( m_pCam, pFrame );
