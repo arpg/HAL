2a0d12ffe9a3f1adfdcf0fdb85716af49c89ab26
Chris Mei <christopher.m.mei@gmail.com>
2012-05-04 15:36:00 +0000

    Added reading function to imagewrapper (reads the property map).
    git-svn-id: svn+ssh://robotics.gwu.edu/home/rpg/repos/RPG/core/RPG@369 e56edf37-dbb1-46d4-901c-b18800ef280d

--- Utils/ImageWrapper.h
+++ Utils/ImageWrapper.h
@@ -6,7 +6,20 @@
 #include <RPG/Utils/PropertyMap.h>
 
 namespace rpg {
+    class ImageWrapper;
+    ////////////////////////////////////////////////////////////////////////////
+    inline ImageWrapper imread( const std::string& sImageFileName, 
+                                const std::string& sExtraInfoFileName,
+                                int nFlags = 1 //<Input: same flag convention as in OpenCV (>0 colour, 0 greyscale, <0 as is)
+                                );
 
+    ////////////////////////////////////////////////////////////////////////////
+    inline ImageWrapper imread( const std::string& sImageFileName, 
+                                bool bReadExtraInfo = true,
+                                int nFlags = 1 //<Input: same flag convention as in OpenCV (>0 colour, 0 greyscale, <0 as is)
+                                );
+
+    ////////////////////////////////////////////////////////////////////////////
     class ImageWrapper {
     public:
         cv::Mat         Image;
@@ -27,9 +40,30 @@ namespace rpg {
 
         /// If bWriteExtraInfo is set to true, this call will automatically deduce the 
         /// sExtraInfoName from the sImageName by replacing the
-        // extension by ".txt" (if not extension is found, ".txt" will be
-        // appended). No checks are made for overwriting.
+        /// extension by ".txt" (if not extension is found, ".txt" will be
+        /// appended). No checks are made for overwriting.
         inline bool write( const std::string& sImageName, bool bWriteExtraInfo = true );
+
+        /// Read an image from sImageName and the property map from sExtraInfoName.
+        /// nFlags can be used to automatically load in color or image (or keep as such - default).
+        inline void read( const std::string& sImageFileName, 
+                          const std::string& sExtraInfoFileName,
+                          int nFlags = 1 ) {
+            *this = imread( sImageFileName, sExtraInfoFileName, nFlags );
+        }
+
+        /// Read an image from sImageName and the property map from sExtraInfoName.
+        /// If bReadExtraInfo is set to true, this call will automatically deduce the 
+        /// sExtraInfoName from the sImageName by replacing the
+        /// extension by ".txt" (if not extension is found, ".txt" will be
+        /// appended).
+        /// nFlags can be used to automatically load in color or image (or keep as such - default).
+        inline void read( const std::string& sImageFileName, 
+                          bool bReadExtraInfo = true,
+                          int nFlags = 1 //<Input: same flag convention as in OpenCV (>0 colour, 0 greyscale, <0 as is)
+                          ) {
+            *this = imread( sImageFileName, bReadExtraInfo, nFlags );
+        }
     };
 
     ////////////////////////////////////////////////////////////////////////////////
@@ -55,6 +89,46 @@ namespace rpg {
         }
         return true;
     }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+inline rpg::ImageWrapper rpg::imread( const std::string& sImageFileName, 
+                                      const std::string& sExtraInfoFileName,
+                                      int nFlags
+                                      ) {
+    ImageWrapper retImage;
+    retImage.Image = cv::imread( sImageFileName.c_str(), nFlags );
+    if( sExtraInfoFileName != "" ) {
+        cv::FileStorage oFile( sExtraInfoFileName.c_str(), cv::FileStorage::READ );
+
+        cv::FileNode r = oFile.root();
+        cv::FileNodeIterator it_r_b = r.begin();
+        cv::FileNodeIterator it_r_e = r.end();
+
+        for( ; it_r_b != it_r_e; it_r_b++ ) {
+            cv::FileNode fnode = *it_r_b;
+            for( cv::FileNodeIterator it = fnode.begin(); it != fnode.end(); it++ ) {
+                //std::cout << (*it).name() << std::endl;
+                //std::cout << (std::string)oFile[ (*it).name() ] << std::endl;
+                retImage.Map.SetProperty( (*it).name(), 
+                                          (std::string)oFile[ (*it).name() ] );
+            }
+        }
+    }
+    return retImage;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+inline rpg::ImageWrapper rpg::imread
+( const std::string& sImageFileName, bool bReadExtraInfo, int nFlags
+  ) {
+    std::string sExtraInfoName = "";
+    if( bReadExtraInfo ) {
+        sExtraInfoName = sImageFileName;
+        sExtraInfoName.erase( sExtraInfoName.rfind( '.' ) );
+        sExtraInfoName += ".txt";
+    }
+    return imread( sImageFileName, sExtraInfoName, nFlags );
 }
 
 #endif	/* RPG_IMAGEWRAPPER_H */
