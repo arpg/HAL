b43bb5c725233210f9cf8be79a7c85735033560f
Gabe Sibley <gsibley@gwu.edu>
2013-04-16 16:38:13 -0400

    removed Common.cmake

--- Devices/Camera/CameraDevice.h
+++ Devices/Camera/CameraDevice.h
@@ -34,7 +34,31 @@ class CameraDevice : public PropertyMap
             }
         }
 
+        /*
         ///////////////////////////////////////////////////////////////
+        bool InitDriver( const std::string& sRobotModelFile, const std::string& sDeviceName )
+        {
+            std::string sWorldModelFile = getenv( "SIMCFG" ); // e.g., world.xml
+            if( !sWorldModelFile.isempty() ){
+                m_pDriver = CreateSimCameraDriver( sDriver, sWorldModelFile, sRobotModelFile );  
+                // CreateSimCameraDriver will:
+                // 1) connect to RobotProxy via node
+                //    (start RobotProxy if not already running, name the proxy after the robot)
+                // 2) ask proxy to start SimCam
+                // 3) then start and connect with a NodeCam
+            }
+            else{
+                m_pDriver = CreateCameraDriver( sDriver );
+            }
+            m_pDriver = CreateCameraDriver( sDriver );
+            if( m_pDriver ){
+                m_pDriver->SetPropertyMap( this );
+                return m_pDriver->Init();
+            }
+            return false;
+        }
+        */
+
         bool InitDriver( const std::string& sDriver )
         {
             if(m_pDriver) {
@@ -42,6 +66,25 @@ class CameraDevice : public PropertyMap
                 m_pDriver = 0;
             }
 
+            /*
+            std::string sWorldModelFile = getenv( "SIMCFG" ); // e.g., world.xml
+            if( !sWorldModelFile.isempty() ){
+                printf("WARNING: you have asked for simulation, but you have not provided"
+                        " a model of the robot.  Please use the other API\n" );
+            }
+
+            std::string sWorldModelFile = getenv( "SIMCFG" ); // e.g., world.xml
+            if( !sWorldModelFile.isempty() ){
+                m_pDriver = CreateSimCameraDriver( sDriver, sWorldModelFile,  ); 
+                // connect to RobotProxy via node
+                //  (start RobotProxy if not already running, name the proxy after the robot)
+                // ask proxy to start SimCam
+                // then start and connect with a NodeCam
+            }
+            else{
+                m_pDriver = CreateCameraDriver( sDriver );
+            }
+            */
             m_pDriver = CreateCameraDriver( sDriver );
             if( m_pDriver ){
                 m_pDriver->SetPropertyMap( this );
