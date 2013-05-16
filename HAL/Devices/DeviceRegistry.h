e29ecb014b10c70e8da391421496df3571a64247
Steven Lovegrove <stevenlovegrove@gmail.com>
2013-05-16 17:20:13 -0400

    Add device aliases feature for common uri's.

--- HAL/Devices/DeviceRegistry.h
+++ HAL/Devices/DeviceRegistry.h
@@ -26,7 +26,11 @@ public:
         std::shared_ptr<BaseDevice> dev;
     };
 
-    DeviceRegistry() = default;
+    DeviceRegistry() {
+        RegisterAlias( "bumblebee", "debayer://dc1394:[]://" );
+        RegisterAlias( "twizzler",  "deinterlace://v4l://" );
+    }
+
     ~DeviceRegistry() = default;
 
     inline static DeviceRegistry<BaseDevice>& I()
@@ -43,34 +47,56 @@ public:
     {
         m_factories[device_name] = factory;
     }
+    
+    void RegisterAlias(
+            const std::string& name,
+            const std::string& alias
+            )
+    {
+        m_aliases[name] = alias;
+    }
 
     std::shared_ptr<BaseDevice> Create(const Uri& uri)
     {
-        auto pf = m_factories.find(uri.scheme);
-        if(pf != m_factories.end()) {
-            std::shared_ptr<BaseDevice> dev = pf->second->GetDevice(uri);
-//            m_instances[uri.scheme].insert( dev );
-            return dev;
+        // Check for aliases
+        std::map<std::string,std::string>::const_iterator iAlias=
+                m_aliases.find(uri.scheme);
+        
+        if(iAlias != m_aliases.end()) {
+            std::string sAlias = iAlias->second;
+            std::ostringstream oss;
+            oss << sAlias << uri.url;
+            return Create(oss.str());
         }else{
-            throw DeviceException("Scheme '" + uri.scheme + "' not registered for factory");
+            auto pf = m_factories.find(uri.scheme);
+            if(pf != m_factories.end()) {
+                std::shared_ptr<BaseDevice> dev = pf->second->GetDevice(uri);
+//                m_instances[uri.scheme].insert( dev );
+                return dev;
+            }else{
+                throw DeviceException("Scheme '" + uri.scheme + "' not registered for factory");
+            }
         }
     }
 
     void Destroy(BaseDevice* dev)
     {
-        auto i = std::find(m_instances.begin(), m_instances.end(), dev);
+//        auto i = std::find(m_instances.begin(), m_instances.end(), dev);
 
-        if(i != m_instances.end()) {
-            m_instances.erase(i);
-        }
+//        if(i != m_instances.end()) {
+//            m_instances.erase(i);
+//        }
     }
 
 protected:
+    // Map of device names to aliases.
+    std::map<std::string,std::string> m_aliases;
+    
     // Map of device name -> factory instance
     std::map<std::string,DeviceFactory<BaseDevice>*> m_factories;
 
-    // Vector of running instances
-    std::vector<DevInstance> m_instances;
+//    // Vector of running instances
+//    std::vector<DevInstance> m_instances;
 };
 
 }
