24c967188aa883810d628e832e9b9e64d0d82535
Steven Lovegrove <stevenlovegrove@gmail.com>
2013-05-16 14:44:09 -0400

    Conditionally compile v4l.

--- HAL/Devices/SharedLoad.h
+++ HAL/Devices/SharedLoad.h
@@ -14,12 +14,14 @@ struct StaticInitForceLoad{
         // TODO: check this path always works out.
         void* handle = dlopen(libpath, RTLD_NOW);
         if(!handle) {
-            std::cerr << "Unable to dlopen " << libpath << std::endl;
+            std::cerr << "Unable to dlopen '" << libpath << "'" << std::endl;
         }
     }
 };
 
+#ifndef __APPLE__
 // Trigger load of all symbols via this static variable
 static StaticInitForceLoad g_StaticInitForceLoadHAL("libhal.so");
+#endif
 
 }
