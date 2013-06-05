be2fa7898f7b131b1e1c19ec4ba07eaf2688ecb9
Steven Lovegrove <stevenlovegrove@gmail.com>
2013-06-05 13:48:32 -0400

    Make raisin driver optional. Make case consistent.

--- HAL/Camera/Drivers/Android/FindRaisin.cmake
+++ HAL/Camera/Drivers/Android/FindRaisin.cmake
@@ -1,56 +1,64 @@
-SET(RAISIN_POSSIBLE_ROOT_DIRS
-	    "$ENV{RAISIN_ROOT_DIR}"                         # *NIX: custom install location (like ROS)
+# FindRaisin.cmake
+#   Raisin_FOUND
+#   Raisin_INCLUDE_DIRS
+#   Raisin_LIBRARIES
+
+SET(Raisin_POSSIBLE_ROOT_DIRS
+	"$ENV{Raisin_ROOT_DIR}"                         # *NIX: custom install location (like ROS)
         /usr/local                                      # Linux: default dir by CMake
         /usr                                            # Linux
         /opt/local                                      # OS X: default MacPorts location
         ${CMAKE_INSTALL_PREFIX}                         # Android toolchain
 )
 
-FIND_PATH(RAISIN_ROOT_INC_DIR
+FIND_PATH(Raisin_ROOT_INC_DIR
           NAMES movidius-validation/validation-suite.h
-          PATHS ${RAISIN_POSSIBLE_ROOT_DIRS}
+          PATHS ${Raisin_POSSIBLE_ROOT_DIRS}
           )
 
 # absolute path to all libraries 
-SET(RAISIN_LIBRARY_SEARCH_PATHS "${RAISIN_ROOT_DIR}/lib")
+SET(Raisin_LIBRARY_SEARCH_PATHS "${Raisin_ROOT_DIR}/lib")
 
-FIND_LIBRARY(RAISIN_VALIDATION_LIBRARY       NAMES validation-suite       PATHS ${RAISIN_LIBRARY_SEARCH_PATHS})
-FIND_LIBRARY(RAISIN_FRAMEDATA_LIBRARY        NAMES frame_data              PATHS ${RAISIN_LIBRARY_SEARCH_PATHS})
+FIND_LIBRARY(Raisin_VALIDATION_LIBRARY       NAMES validation-suite       PATHS ${Raisin_LIBRARY_SEARCH_PATHS})
+FIND_LIBRARY(Raisin_FRAMEDATA_LIBRARY        NAMES frame_data              PATHS ${Raisin_LIBRARY_SEARCH_PATHS})
 
-SET(RAISIN_INCLUDE_DIRS
-    ${RAISIN_VALIDATION_INCLUDE_DIR}
-    ${RAISIN_FRAMEDATA_INCLUDE_DIR}
+SET(Raisin_INCLUDE_DIRS
+    ${Raisin_VALIDATION_INCLUDE_DIR}
+    ${Raisin_FRAMEDATA_INCLUDE_DIR}
     )
 
-SET(RAISIN_LIBRARIES
-    ${RAISIN_VALIDATON_LIBRARY}
-    ${RAISIN_FRAMEDATA_LIBRARY}
+SET(Raisin_LIBRARIES
+    ${Raisin_VALIDATON_LIBRARY}
+    ${Raisin_FRAMEDATA_LIBRARY}
     )
 
 
-SET(RAISIN_FOUND ON)
-FOREACH(NAME ${RAISIN_INCLUDE_DIRS})
+SET(Raisin_FOUND ON)
+FOREACH(NAME ${Raisin_INCLUDE_DIRS})
     IF(NOT EXISTS ${NAME})
-        SET(RAISIN_FOUND OFF)
+        SET(Raisin_FOUND OFF)
     ENDIF(NOT EXISTS ${NAME})
 ENDFOREACH(NAME)
-FOREACH(NAME ${RAISIN_LIBRARIES})
+FOREACH(NAME ${Raisin_LIBRARIES})
     IF(NOT EXISTS ${NAME})
-        SET(RAISIN_FOUND OFF)
+        SET(Raisin_FOUND OFF)
     ENDIF(NOT EXISTS ${NAME})
 ENDFOREACH(NAME)
 
 MARK_AS_ADVANCED(FORCE
-                 RAISIN_ROOT_DIR
-                 RAISIN_VALIDATION_INCLUDE_DIR
-                 RAISIN_FRAMEDATA_INCLUDE_DIR
-                 RAISIN_VALIDATION_LIBRARY
-                 RAISIN_FRAMEDATA_LIBRARY
+                 Raisin_ROOT_DIR
+                 Raisin_VALIDATION_INCLUDE_DIR
+                 Raisin_FRAMEDATA_INCLUDE_DIR
+                 Raisin_VALIDATION_LIBRARY
+                 Raisin_FRAMEDATA_LIBRARY
                  )
 
-# display help message
-IF(NOT RAISIN_FOUND)
-    # make FIND_PACKAGE friendly
-    MESSAGE(FATAL_ERROR "RAISIN not found. Please specify it's location with the RAISIN_ROOT_DIR env. variable.")
-ENDIF(NOT RAISIN_FOUND)
-
+IF(Raisin_FOUND)
+   IF(NOT Raisin_FIND_QUIETLY)
+      MESSAGE(STATUS "Found Raisin: ${Raisin_LIBRARY}")
+   ENDIF (NOT Raisin_FIND_QUIETLY)
+ELSE(Raisin_FOUND)
+   IF(Raisin_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "Could not find Raisin. Please specify it's location with the Raisin_ROOT_DIR env. variable.")
+   ENDIF(Raisin_FIND_REQUIRED)
+ENDIF(Raisin_FOUND)
