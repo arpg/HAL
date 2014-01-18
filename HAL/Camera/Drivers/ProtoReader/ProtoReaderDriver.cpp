a5da1c388cacbf9163c25527e85cca8aba4c5e88
Jack Morrison <jackmorrison1@gmail.com>
2014-01-18 16:24:02 -0500

    Googlifies ProtoReaderDriver.

--- HAL/Camera/Drivers/ProtoReader/ProtoReaderDriver.cpp
+++ HAL/Camera/Drivers/ProtoReader/ProtoReaderDriver.cpp
@@ -5,87 +5,68 @@
 #include <iostream>
 #include <unistd.h>
 
-namespace hal
-{
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace hal {
 ProtoReaderDriver::ProtoReaderDriver(std::string filename, size_t imageID)
     : m_first(true),
-      m_reader( pb::Reader::Instance(filename,pb::Msg_Type_Camera) )
-{
-    m_reader.SetInitialImage(imageID);
-    while( !ReadNextCameraMessage(m_nextMsg) ) {
-        std::cout << "HAL: Initializing proto-reader..." << std::endl;
-        usleep(100);
-    }
+      m_reader( pb::Reader::Instance(filename,pb::Msg_Type_Camera) ) {
+  m_reader.SetInitialImage(imageID);
+  while( !ReadNextCameraMessage(m_nextMsg) ) {
+    std::cout << "HAL: Initializing proto-reader..." << std::endl;
+    usleep(100);
+  }
 
-    const pb::Header pbHdr = m_reader.GetHeader();
-    time_t log_date((long)pbHdr.date());
-    std::cout << "- Log dated " << ctime(&log_date);
+  const pb::Header pbHdr = m_reader.GetHeader();
+  time_t log_date((long)pbHdr.date());
+  std::cout << "- Log dated " << ctime(&log_date);
 
-    m_numChannels = m_nextMsg.image_size();
-    for(size_t c=0; c < m_numChannels; ++c) {
-        m_width.push_back(m_nextMsg.image(c).width());
-        m_height.push_back(m_nextMsg.image(c).height());
-    }
+  m_numChannels = m_nextMsg.image_size();
+  for(size_t c=0; c < m_numChannels; ++c) {
+    m_width.push_back(m_nextMsg.image(c).width());
+    m_height.push_back(m_nextMsg.image(c).height());
+  }
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-ProtoReaderDriver::~ProtoReaderDriver()
-{
-//    m_reader.StopBuffering();
+ProtoReaderDriver::~ProtoReaderDriver() {
+  //    m_reader.StopBuffering();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-bool ProtoReaderDriver::ReadNextCameraMessage(pb::CameraMsg& msg)
-{
-    msg.Clear();
-    std::unique_ptr<pb::CameraMsg> readmsg = m_reader.ReadCameraMsg();
-    if(readmsg) {
-        msg.Swap(readmsg.get());
-        return true;
-    }else{
-        return false;
-    }
+bool ProtoReaderDriver::ReadNextCameraMessage(pb::CameraMsg& msg) {
+  msg.Clear();
+  std::unique_ptr<pb::CameraMsg> readmsg = m_reader.ReadCameraMsg();
+  if(readmsg) {
+    msg.Swap(readmsg.get());
+    return true;
+  }else{
+    return false;
+  }
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-bool ProtoReaderDriver::Capture( pb::CameraMsg& vImages )
-{
-    if( m_first ) {
-        m_nextMsg.Swap(&vImages);
-        m_first = false;
-    } else {
-        ReadNextCameraMessage(vImages);
-    }
-    return vImages.image_size() > 0;
+bool ProtoReaderDriver::Capture( pb::CameraMsg& vImages ) {
+  if( m_first ) {
+    m_nextMsg.Swap(&vImages);
+    m_first = false;
+  } else {
+    ReadNextCameraMessage(vImages);
+  }
+  return vImages.image_size() > 0;
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-std::string ProtoReaderDriver::GetDeviceProperty(const std::string& sProperty)
-{
-    if(sProperty == hal::DeviceDirectory) {
-        return DirUp(m_reader.GetFilename());
-    }
-    return std::string();
+std::string ProtoReaderDriver::GetDeviceProperty(const std::string& sProperty) {
+  if(sProperty == hal::DeviceDirectory) {
+    return DirUp(m_reader.GetFilename());
+  }
+  return std::string();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-size_t ProtoReaderDriver::NumChannels() const
-{
-    return m_numChannels;
+size_t ProtoReaderDriver::NumChannels() const {
+  return m_numChannels;
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-size_t ProtoReaderDriver::Width( size_t idx ) const
-{
-    return m_width[idx];
+size_t ProtoReaderDriver::Width( size_t idx ) const {
+  return m_width[idx];
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-size_t ProtoReaderDriver::Height( size_t idx ) const
-{
-    return m_height[idx];
+size_t ProtoReaderDriver::Height( size_t idx ) const {
+  return m_height[idx];
 }
-
-}
+}  // end namespace hal
--- HAL/Camera/Drivers/ProtoReader/ProtoReaderDriver.h
+++ HAL/Camera/Drivers/ProtoReader/ProtoReaderDriver.h
@@ -3,37 +3,35 @@
 #include <HAL/Camera/CameraDriverInterface.h>
 #include <PbMsgs/Reader.h>
 
-namespace hal
-{
+namespace hal {
 
-class ProtoReaderDriver : public CameraDriverInterface
-{
-public:
-    ProtoReaderDriver(std::string filename, size_t imageID);
-    ~ProtoReaderDriver();
+class ProtoReaderDriver : public CameraDriverInterface {
+ public:
+  ProtoReaderDriver(std::string filename, size_t imageID);
+  ~ProtoReaderDriver();
 
-    bool Capture( pb::CameraMsg& vImages );
+  bool Capture( pb::CameraMsg& vImages );
 
-    std::shared_ptr<CameraDriverInterface> GetInputDevice() {
-        return std::shared_ptr<CameraDriverInterface>();
-    }
+  std::shared_ptr<CameraDriverInterface> GetInputDevice() {
+    return std::shared_ptr<CameraDriverInterface>();
+  }
 
-    std::string GetDeviceProperty(const std::string& sProperty);
+  std::string GetDeviceProperty(const std::string& sProperty);
 
-    size_t NumChannels() const;
-    size_t Width( size_t /*idx*/ = 0 ) const;
-    size_t Height( size_t /*idx*/ = 0 ) const;
+  size_t NumChannels() const;
+  size_t Width( size_t /*idx*/ = 0 ) const;
+  size_t Height( size_t /*idx*/ = 0 ) const;
 
-protected:
-    bool ReadNextCameraMessage(pb::CameraMsg& msg);
+ protected:
+  bool ReadNextCameraMessage(pb::CameraMsg& msg);
 
-    bool                    m_first;
-    pb::Reader&             m_reader;
-    pb::CameraMsg           m_nextMsg;
+  bool                    m_first;
+  pb::Reader&             m_reader;
+  pb::CameraMsg           m_nextMsg;
 
-    std::vector<size_t>     m_width;
-    std::vector<size_t>     m_height;
-    size_t                  m_numChannels;
+  std::vector<size_t>     m_width;
+  std::vector<size_t>     m_height;
+  size_t                  m_numChannels;
 };
 
-} /* namespace */
+}  // end namespace hal
