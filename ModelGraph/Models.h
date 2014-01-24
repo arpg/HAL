e640fc5063a2d5d91f735aee7f4fbfd99b30c71f
Brandon Minor <bminortx@gwmail.gwu.edu>
2014-01-24 14:31:08 -0500

    Got rid of ModelGraph; now found entirely in SimBA

--- ModelGraph/Models.h
+++
@@ -1,6 +0,0 @@
-#include <ModelGraph/Model.h>
-#include <ModelGraph/Body.h>
-#include <ModelGraph/Link.h>
-#include <ModelGraph/Joint.h>
-#include <ModelGraph/ModelNode.h>
-#include <ModelGraph/Shape.h>
