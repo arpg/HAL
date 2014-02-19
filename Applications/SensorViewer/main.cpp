48217440ca5716361556b436c3b1fb05435c24e9
Sina Aghli <aghli@gwmail.com>
2014-02-19 17:12:22 -0500

    adding odometry to sensorviewer

--- Applications/SensorViewer/main.cpp
+++ Applications/SensorViewer/main.cpp
@@ -190,6 +190,12 @@ class SensorViewer {
     has_posys_ = true;
   }
 
+  // osme more change
+  void set_odometry(const std::string& odometry_uri){
+    osometry_ = hall::Posys(odometry_uri);
+    has_odometry_ = true;
+  }
+
  protected:
   void RegisterCallbacks() {
     if (has_posys_) {
