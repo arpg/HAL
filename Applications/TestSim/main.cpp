caf6c07e79acd42099fd890b125225a746184236
Laxit Gavshinde <laxitg@gwmail.gwu.edu>
2014-03-04 17:10:21 -0500

    TestSim Added

---
+++ Applications/TestSim/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+#include <HAL/Camera/CameraDevice.h>
+#include <HAL/Car/CarDevice.h>
+
+#include <pangolin/pangolin.h>
+
+int main(/*int argc, char **argv*/)
+{
+  hal::Camera cam("openni:[rgb=1,depth=1]//");
+  hal::Car car("car://");//Uri doesn't has any significance.
+  int winw = cam.NumChannels() * cam.Width();
+  int winh = cam.Height();
+
+  pangolin::CreateWindowAndBind("TestSim", winw, winh);
+  glPixelStorei(GL_PACK_ALIGNMENT, 1);
+  glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+
+  pangolin::View& container = pangolin::CreateDisplay()
+                                        .SetLayout(pangolin::LayoutEqual);
+  for (int display = 0; display < cam.NumChannels(); ++display)
+  {
+    container.AddDisplay(pangolin::CreateDisplay()
+                         .SetAspect(cam.Width()/(double)cam.Height()));
+  }
+  std::cout<<"num ch = "<<cam.NumChannels()<<std::endl;
+
+  pangolin::GlTexture tex[cam.NumChannels()];
+  bool run = true;
+  std::shared_ptr<pb::ImageArray> imgs = pb::ImageArray::Create();
+  for (int frame = 0; !pangolin::ShouldQuit(); ++frame)
+  {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3f(1,1,1);
+
+    if(run) {
+        if( !cam.Capture( *imgs) ) {
+            run = false;
+        }
+    }
+
+    for(size_t i=0; i<imgs->Size(); ++i ) {
+      pb::Image img = *(imgs->at(i));
+      if(!tex[i].tid) {
+        tex[i].Reinitialise(img.Width(), img.Height(),
+                              img.Format(), true, 0,
+                              img.Format(), img.Type(), 0);
+      }
+        container[i].Activate();
+        tex[i].Upload(
+            img.data(),
+            img.Format(), img.Type()
+        );
+        tex[i].RenderToViewportFlipY();
+    }
+    pangolin::FinishFrame();
+  }
+}
