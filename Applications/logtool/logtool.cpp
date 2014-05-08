4d875dda8f37b8dff331dc300d87a161e859f1d2
Jack Morrison <jackmorrison1@gmail.com>
2014-05-08 00:36:53 -0400

    Adds extract functionality to logtool.

--- Applications/logtool/logtool.cpp
+++ Applications/logtool/logtool.cpp
@@ -1,5 +1,7 @@
 #include <gflags/gflags.h>
 #include <miniglog/logging.h>
+#include <PbMsgs/Logger.h>
+#include <PbMsgs/Reader.h>
 
 /**
  * logtool functionality
@@ -21,16 +23,95 @@ DEFINE_string(extract_types, "",
               "Comma-separated list of types to extract from log. "
               "Options include \"cam\", \"imu\", and \"posys\".");
 DEFINE_string(extract_frame_range, "",
-              "Range of image frames to extract from the log. "
+              "Range (inclusive) of image frames to extract from the log. "
               "Should be a comma-separated pair, e.g \"0,200\"");
 
-void extract() {
+template <typename T>
+inline void Split(const std::string& s, char delim,
+                  std::vector<T>* elems) {
+  std::stringstream ss(s);
+  std::string item;
+  while (std::getline(ss, item, delim)) {
+    T val;
+    std::stringstream val_ss(item);
+    val_ss >> val;
+    elems->push_back(val);
+  }
+}
+
+template<>
+inline void Split<std::string>(const std::string& s, char delim,
+                               std::vector<std::string>* elems) {
+  std::stringstream ss(s);
+  std::string item;
+  while (std::getline(ss, item, delim)) {
+    elems->push_back(item);
+  }
+}
+
+inline pb::MessageType MsgTypeForString(const std::string& str) {
+  static std::map<std::string, pb::MessageType> kTypeStrings = {
+    {"cam", pb::Msg_Type_Camera},
+    {"imu", pb::Msg_Type_IMU},
+    {"encoder", pb::Msg_Type_Encoder},
+    {"lidar", pb::Msg_Type_LIDAR},
+    {"posys", pb::Msg_Type_Posys},
+  };
+  auto it = kTypeStrings.find(str);
+  CHECK_NE(it, kTypeStrings.end()) << "Type string '" << str
+                                   << "' does not match a known data type";
+  return it->second;
+}
+
+void Extract() {
+  static const int kNoRange = -1;
   // Parse types to extract
+  std::vector<std::string> types;
+  Split(FLAGS_extract_types, ',', &types);
+
   // Parse frame range to extract or set to -1
+  int frame_min = kNoRange, frame_max = kNoRange;
+  std::vector<int> frames;
+  Split(FLAGS_extract_frame_range, ',', &frames);
+  if (!frames.empty()) {
+    CHECK_EQ(2, frames.size()) << "extract_frame_range must be frame PAIR";
+    frame_min = frames[0];
+    frame_max = frames[1];
+    CHECK_LT(frame_min, frame_max)
+        << "Minimum frame index must be less than max frame.";
+  }
+
   // Open the input file
+  pb::Reader reader(FLAGS_in);
+  if (FLAGS_extract_types.empty()) {
+    reader.EnableAll();
+  } else {
+    LOG(INFO) << "Extracting: ";
+    for (const std::string& type : types) {
+      LOG(INFO) << "\t" << type;
+      reader.Enable(MsgTypeForString(type));
+    }
+  }
+
   // Seek to range min
+  int idx = 0;
+  for (; frame_min != kNoRange && idx < frame_min; ++idx) {
+    reader.ReadCameraMsg();
+  }
+
+  pb::Logger logger;
+  logger.LogToFile(FLAGS_out);
+
   // While not at the range max
-  // If we can get a message of one of the right types
+  std::unique_ptr<pb::Msg> msg;
+  while ((frame_max == kNoRange ||
+          idx <= frame_max) &&
+         (msg = reader.ReadMessage())) {
+    if (msg->has_camera()) {
+      ++idx;
+    }
+    while (!logger.LogMessage(*msg)) {}
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -38,7 +119,9 @@ int main(int argc, char *argv[]) {
   google::InitGoogleLogging(argv[0]);
 
   if (FLAGS_extract) {
-    extract();
+    CHECK(!FLAGS_in.empty()) << "Input file required for extraction.";
+    CHECK(!FLAGS_out.empty()) << "Output file required for extraction.";
+    Extract();
   }
 
   return 0;
