35cd59a840da1fc46a0d0dc985d25f89c47548db
Juan Falquez <jfalquez@gwu.edu>
2014-05-13 09:04:59 -0400

    Added GIT check. Moved include to top of file.

--- CMakeModules/set_git_version.cmake
+++ CMakeModules/set_git_version.cmake
@@ -2,9 +2,16 @@
  # get_git_version.cmake - Function for setting up versions based on GIT.
  #
  # get_git_version - Takes a variable name and sets it to the GIT tag version.
+ #
+ # If GIT package is not found, sets default version to 1.0.0
  #######################################################################################
 
 function(set_git_version var)
-  execute_process(COMMAND git describe OUTPUT_VARIABLE GIT_TAG_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(${var} ${GIT_TAG_VERSION} CACHE INTERNAL "Version Number" FORCE)
+  find_package(GIT QUIET)
+  if(GIT_FOUND)
+    execute_process(COMMAND ${GIT_EXECUTABLE} describe OUTPUT_VARIABLE GIT_TAG_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+    set(${var} ${GIT_TAG_VERSION} CACHE INTERNAL "Version Number" FORCE)
+  else()
+    set(${var} "1.0.0" CACHE INTERNAL "Version Number" FORCE)
+  endif()
 endfunction()
