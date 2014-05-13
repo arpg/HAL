75af71b5c315f690d8c913741b20d93b767a6769
Juan Falquez <jfalquez@gwu.edu>
2014-05-13 09:55:53 -0400

    More checks on cmake script.

--- CMakeModules/set_git_version.cmake
+++ CMakeModules/set_git_version.cmake
@@ -9,9 +9,18 @@
 function(set_git_version var)
   find_package(GIT QUIET)
   if(GIT_FOUND)
-    execute_process(COMMAND ${GIT_EXECUTABLE} describe OUTPUT_VARIABLE GIT_TAG_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
-    set(${var} ${GIT_TAG_VERSION} CACHE INTERNAL "Version Number" FORCE)
+    execute_process(COMMAND ${GIT_EXECUTABLE} describe --abbrev=0
+                    RESULT_VARIABLE GIT_TAG_RESULT
+                    OUTPUT_VARIABLE GIT_TAG_VERSION
+                    OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+    if(GIT_TAG_RESULT EQUAL 0)
+      set(${var} ${GIT_TAG_VERSION} CACHE INTERNAL "Version Number" FORCE)
+    else()
+      set(${var} "1.0.0" CACHE INTERNAL "Version Number" FORCE)
+      message(STATUS "GIT not found! ${var} set to default value: ${${var}}")
+    endif()
   else()
     set(${var} "1.0.0" CACHE INTERNAL "Version Number" FORCE)
+    message(STATUS "GIT not found! ${var} set to default value: ${${var}}")
   endif()
 endfunction()
