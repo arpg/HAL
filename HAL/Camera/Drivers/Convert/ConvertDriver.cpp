e92d9b06822808762bde6d301e1e72807337cc42
Dorian Galvez-Lopez <dorian@email.gwu.edu>
2014-07-22 14:29:01 -0400

    Convert driver propagates camera serial number

--- HAL/Camera/Drivers/Convert/ConvertDriver.cpp
+++ HAL/Camera/Drivers/Convert/ConvertDriver.cpp
@@ -69,6 +69,7 @@ bool ConvertDriver::Capture( pb::CameraMsg& vImages )
     pbImg->set_format( pb::PB_LUMINANCE );
     pbImg->mutable_data()->resize(m_nImgWidth*m_nImgHeight);
     pbImg->set_timestamp( m_Message.image(ii).timestamp() );
+    pbImg->set_serialno( m_Message.image(ii).serialno() );
 
     cv::Mat sImg(m_nImgHeight, m_nImgWidth, m_nCvType,
                    (void*)m_Message.mutable_image(ii)->data().data());
