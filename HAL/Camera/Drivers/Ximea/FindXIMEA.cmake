4a393cdfec98386ecccc2cc856a4fd7c0a7f1942
Sina Aghli <Sina@engr2-14-161-dhcp.int.colorado.edu>
2014-10-20 16:34:36 -0600

    changes to find Ximea libs

--- HAL/Camera/Drivers/Ximea/FindXIMEA.cmake
+++ HAL/Camera/Drivers/Ximea/FindXIMEA.cmake
@@ -1,49 +1,31 @@
 #
-#  Try to find ximea library
-# 
-#  This module defines the following variables:
-#      XIMEA_FOUND       -
-#      XIMEA_INCLUDE_DIR - include directory
-#      XIMEA_LIBRARIES   - full path libraries
-#   
+##  Try to find ximea library
+##
+##  This module defines the following variables:
+##      XIMEA_FOUND       -
+##      XIMEA_INCLUDE_DIR - include directory
+##      XIMEA_LIBRARIES   - full path libraries
+##
 
+SET( XIMEA_SEARCH_PATHS
+    /opt/XIMEA/lib
+    /opt/XIMEA/include
+    /Library/Frameworks/m3api.framework/Libraries
+)
 
-IF( NOT XIMEA_FOUND )
-  # One variable to hold all the paths that are used for searching below.
-  # Easier to maintain, to debug and to extend...
-  # Note, CMake does not complain about path that do not exist...
-  SET( XIMEA_SEARCH_PATHS
-        /opt/XIMEA/lib 
-        /opt/XIMEA/include
-        ${CMAKE_FRAMEWORK_PATH}/Versions/Current/Headers
-        ${CMAKE_FRAMEWORK_PATH}/Versions/Current/Libraries
-     )
+find_path(XIMEA_INCLUDE_DIR
+    NAMES xiApi.h
+    PATHS ${XIMEA_SEARCH_PATHS}
+)
 
-  # Common for both UNIX and APPLE.
-  FIND_LIBRARY( XIMEA_LIBRARY NAMES m3api PATHS ${XIMEA_SEARCH_PATHS} )
-  FIND_PATH( XIMEA_INCLUDE_DIR xiApi.h ${XIMEA_SEARCH_PATHS} )
+find_library( XIMEA_LIBRARY
+    NAMES libXIMEA_GenTL.dylib
+    PATHS ${XIMEA_SEARCH_PATHS}
+)
 
-IF( NOT ${XIMEA_LIBRARY} STREQUAL "XIMEA_LIBRARY-NOTFOUND" )
-
-    SET( XIMEA_FOUND TRUE )
-
-  ENDIF( NOT ${XIMEA_LIBRARY} STREQUAL "XIMEA_LIBRARY-NOTFOUND" )
-
-  # Only advance if successful...
-  IF( XIMEA_FOUND )
-      # Nice to have.
-      SET(XIMEA_FOUND true CACHE internal "" )
-      SET(XIMEA_LIBRARIES ximea CACHE STRING "Libraries needed for ximea cameras to work." FORCE)
-      MARK_AS_ADVANCED(
-          XIMEA_LIBRARY
-          XIMEA_FOUND
-          XIMEA_INCLUDE_DIR
-          XIMEA_LIBRARIES
-      )
-  ELSE(XIMEA_FOUND)
-      SET(XIMEA_FOUND false CACHE internal "" )
-  ENDIF(XIMEA_FOUND)
-
-ENDIF( NOT XIMEA_FOUND )
-
+if( XIMEA_INCLUDE_DIR AND XIMEA_LIBRARY )
+    set( XIMEA_LIBRARIES ${XIMEA_LIBRARY} )
+    set( XIMEA_INCLUDE_DIRS ${XIMEA_INCLUDE_DIR} )
+    set( XIMEA_FOUND TRUE )
+endif()
 
