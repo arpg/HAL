014888d202cc6f51082a532dcae7732e3d201063
Dorian Galvez-Lopez <dorian@email.gwu.edu>
2014-11-07 00:24:20 -0700

    Join driver

---
+++ HAL/Camera/Drivers/Join/JoinDriver.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <vector>
+#include <list>
+#include <memory>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+#include "HAL/Camera/CameraDriverInterface.h"
+
+namespace hal {
+
+class JoinDriver : public CameraDriverInterface
+{
+public:
+  JoinDriver(
+      const std::vector<std::shared_ptr<CameraDriverInterface>>& cameras);
+  virtual ~JoinDriver();
+
+  bool Capture( pb::CameraMsg& vImages );
+  std::shared_ptr<CameraDriverInterface> GetInputDevice() {
+    return std::shared_ptr<CameraDriverInterface>();
+  }
+
+  std::string GetDeviceProperty(const std::string& sProperty);
+
+  size_t NumChannels() const;
+  size_t Width( size_t idx = 0 ) const;
+  size_t Height( size_t idx = 0 ) const;
+
+private:
+  std::vector<std::shared_ptr<CameraDriverInterface>> m_Cameras;
+  std::vector<unsigned int> m_nImgWidth;
+  std::vector<unsigned int> m_nImgHeight;
+  unsigned int              m_nNumChannels;
+
+  class WorkTeam
+  {
+  public:
+    WorkTeam(){}
+    void addWorker(std::shared_ptr<CameraDriverInterface>& cam);
+    std::vector<pb::CameraMsg>& process();
+
+  private:
+    void waitForWork(size_t workerId);
+    void workerDone(size_t workerId);
+    void Worker(std::shared_ptr<CameraDriverInterface>& cam,
+                size_t workerId);
+
+  private:
+    std::vector<std::thread> m_Workers;
+    std::vector<bool> m_bWorkerDone;
+    std::vector<pb::CameraMsg> m_ImageData;
+    std::mutex m_Mutex;
+    std::condition_variable m_WorkerCond, m_MasterCond;
+  };
+
+  WorkTeam                  m_WorkTeam;
+
+};
+
+}
