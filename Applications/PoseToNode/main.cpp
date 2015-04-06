7436fdbf3e8c6cfcb5da37131be93f5116fa4ba3
Sina Aghli <Sina@sinas-mbp.lan>
2015-04-06 12:35:00 -0600

    some changes

--- Applications/PoseToNode/main.cpp
+++ Applications/PoseToNode/main.cpp
@@ -10,13 +10,13 @@
 #include <HAL/Posys/PosysDevice.h>
 #include <Node/Node.h>
 #include <PbMsgs/Pose.pb.h>
-//#include <miniglog/logging.h>
+#include <miniglog/logging.h>
 
 DEFINE_string(nodeName, "posetonode", "Node name");
 DEFINE_string(topic, "pose", "Topic name");
 DEFINE_int32(verbosity, 2, "Verbosity level of Node "
                            "(the lower the less verbose)");
-DEFINE_string(pose, "", "Pose driver");
+DEFINE_string(posys, "", "Posys driver");
 
 //Global Node so that the callback has access to it
 node::node n;
@@ -38,12 +38,12 @@ void Posys_Handler(pb::PoseMsg& PoseData) {
 int main(int argc, char *argv[])
 {
   // get params
-  gflags::SetUsageMessage("Read poses from a HAL driver and send them over "
+  google::SetUsageMessage("Read poses from a HAL driver and send them over "
                           "the network through Node. Runs as fast as possible.");
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   
-  if(FLAGS_pose.empty())
+  if(FLAGS_posys.empty())
     {
       LOG(FATAL) << "Pose URI not provided. Use parameter -pose";
       return -1;
@@ -52,10 +52,9 @@ int main(int argc, char *argv[])
   // initiate pose provider
   std::unique_ptr<hal::Posys> poseDevice;
   try {
-    poseDevice.reset(new hal::Posys(FLAGS_pose));
+    poseDevice.reset(new hal::Posys(FLAGS_posys));
   } catch (...) {
-    LOG(FATAL) << "Could not create pose system from URI: " << FLAGS_pose;
-    return -1;
+    LOG(FATAL) << "Could not create pose system from URI: " << FLAGS_posys;
   }
 
   // initiate node
