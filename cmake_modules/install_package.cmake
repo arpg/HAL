a1c8ca245a3e6b6de0a375afdb0c1341296849d7
crheckman <crheckman@gmail.com>
2015-04-24 12:20:06 -0600

    include correct directories for HAL when EXPORT_HAL=ON

--- cmake_modules/install_package.cmake
+++ cmake_modules/install_package.cmake
@@ -222,8 +222,8 @@ function(install_package)
         #    endforeach()
         #endif()
 
-        list( APPEND PACKAGE_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/include
-            ${CMAKE_CURRENT_BINARY_DIR}/include )
+        list( APPEND PACKAGE_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}
+            ${CMAKE_CURRENT_BINARY_DIR} )
 
         # install library itself
         #if( PACKAGE_LIB_NAME )
