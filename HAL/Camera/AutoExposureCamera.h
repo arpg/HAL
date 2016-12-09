73dd8a3d81a918cdac8b12020e74abde88239c31
Mike Kasper <michael.kasper@colorado.edu>
2016-12-09 14:23:24 -0700

    removed const modifiers

--- HAL/Camera/AutoExposureCamera.h
+++ HAL/Camera/AutoExposureCamera.h
@@ -9,25 +9,25 @@ class AutoExposureCamera : public CameraDriverInterface
 {
   public:
 
-    virtual int GetAutoExposureImageIndex() const = 0;
+    virtual int GetAutoExposureImageIndex() = 0;
 
     virtual void DisableAutoExposure() = 0;
 
-    virtual double GetActualFramerate() const = 0;
+    virtual double GetActualFramerate() = 0;
 
-    virtual double GetDesiredFramerate() const = 0;
+    virtual double GetDesiredFramerate() = 0;
 
-    virtual double GetExposure() const = 0;
+    virtual double GetExposure() = 0;
 
     virtual void SetExposure(double exposure) = 0;
 
-    virtual double GetGain() const = 0;
+    virtual double GetGain() = 0;
 
     virtual void SetGain(double gain) = 0;
 
-    virtual double GetMaxGain() const = 0;
+    virtual double GetMaxGain() = 0;
 
-    virtual double GetMinGain() const = 0;
+    virtual double GetMinGain() = 0;
 };
 
 } // namespace hal
